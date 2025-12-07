902d4eea5878b3287e0aac63ee21989b0419cc9a
Dana Chavez <danacg11@hotmail.com>
2025-12-07 03:56:31 -0700

    Inicio CU

---
+++ ConfigurarPartidaMVC/src/main/java/itson/rummypresentacion/ConfigurarPartidaMVC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.rummypresentacion;
+
+/**
+ *
+ * @author Dana Chavez
+ */
+public class ConfigurarPartidaMVC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
